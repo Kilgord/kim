8d993bbab5cacbbf2851631201817a92a0571806
Kilgord <54819029+Kilgord@users.noreply.github.com>
2019-09-12 00:38:51 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -1,110 +1,68 @@
-import requests
-import bs4
-import urllib.request
-import re
-from datetime import datetime
-class VkBot:
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class Calculator
+    {
+        public int a;
+        public int b;
+
+
+
+        public static int Summa(int a, int b)
+        {
+           
+            
+            
+            
+            return a+b;
+        }
+        public static int mi(int a, int b)
+        {
+            
+            return a - b;
+        }
+        public static int umn(int a, int b)
+        {
+
+
+           
+            
+            return a*b;
+        }
+
+        public static int del(int a, int b)
+        {
+
+
+            
+            
+            return a/b;
+        }
+        
+    }
+
+
     
-   
- 
-     
+        
+        
+    class Program
+    {
+        
 
-    def __init__(self, user_id):
-    
-        print("Создан объект бота!")
-        self._USER_ID = user_id
-        self._USERNAME = self._get_user_name_from_vk_id(user_id)
         
-        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ВРЕМЯ", "ПОКА", "КНОПКИ"]
+        static void Main(string[] args)
+        {
+            
+            
+            System.Console.WriteLine(Calculator.mi(2, 2), Calculator.del(2, 2),  Calculator.umn(2, 2),  Calculator.Summa(2, 2));
+            Console.ReadKey();
 
-
-     
-    def _get_user_name_from_vk_id(self, user_id):
-        request = requests.get("https://vk.com/id"+str(user_id))
-        bs = bs4.BeautifulSoup(request.text, "html.parser")
-        
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
-        
-        return user_name.split()[0]
-
-    def _get_keyboard(self):
-        keyboard = open("keyboard.json", "r", encoding = "UTF-8").read()
-        return keyboard
-        
-    # Получение времени:
-    def _get_time(self):
-        request = requests.get("https://my-calend.ru/date-and-time-today")
-        b = bs4.BeautifulSoup(request.text, "html.parser")
-        return self._clean_all_tag_from_str(str(b.select(".page")[0].findAll("h2")[1])).split()[1]
-
-    # Получение погоды
-    def _get_weather(self) -> list:
-        
-        request = requests.get("https://sinoptik.com.ru/погода-санкт-петербург" )
-        b = bs4.BeautifulSoup(request.text, "html.parser")
-        print(request)
-        p3 = b.select('.temperature .p3')
-        weather1 = p3[0].getText()
-        p4 = b.select('.temperature .p4')
-        weather2 = p4[0].getText()
-        p5 = b.select('.temperature .p5')
-        weather3 = p5[0].getText()
-        p6 = b.select('.temperature .p6')
-        weather4 = p6[0].getText()
-        result = ''
-        result = result + ('Утром :' + weather1 + ' ' + weather2) + '\n'
-        result = result + ('Днём :' + weather3 + ' ' + weather4) + '\n'
-        temp = b.select('.rSide .description')
-        weather = temp[0].getText()
-        result = result + weather.strip()
-        if weather.find ('дождь'):
-           return result + f"\n\n {self._USERNAME}, не стоит летать в дождь, я вас предупредил" 
-        else:
-            return result + f"\nСтоит полетать"
-        
-     # Метод для очистки от ненужных тэгов
-     
-    @staticmethod
-    def _clean_all_tag_from_str(string_line):
-        """
-        Очистка строки stringLine от тэгов и их содержимых
-        :param string_line: Очищаемая строка
-        :return: очищенная строка
-        """
-        result = ""
-        not_skip = True
-        for i in list(string_line):
-            if not_skip:
-                if i == "<":
-                    not_skip = False
-                else:
-                    result += i
-            else:
-                if i == ">":
-                    not_skip = True
-        
-        return result
-
-    def new_message(self, message):
-
-        # Привет
-        if message.upper() == self._COMMANDS[0]:
-            return f"Привет-привет, {self._USERNAME}!"
-        
-        # Погода
-        elif message.upper() == self._COMMANDS[1]:
-            return self._get_weather()
-        
-        # Время
-        elif message.upper() == self._COMMANDS[2]:
-            return self._get_time()
-        
-        # Пока
-        elif message.upper() == self._COMMANDS[3]:
-            return f"Пока-пока, {self._USERNAME}!"
-        # Кнопки
-        elif message.upper() == self._COMMANDS[4]:
-            return self._get_keyboard()
-        else:
-            return "Не понимаю о чем вы..."
+        }
+    }
+}
 
