5d0acfe77c53ad51a69b632c9d9663d5e1525f20
Kilgord <54819029+Kilgord@users.noreply.github.com>
2019-09-21 01:08:48 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -1,70 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp11
-{
-    public class Calculator
-    {
-        
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_bot import VkBot
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
-
-        public static int Summa(int a, int b)
-        {
-           
-            
-            
-            
-            return a+b;
-        }
-        public static int mi(int a, int b)
-        {
-            
-            return a - b;
-        }
-        public static int umn(int a, int b)
-        {
-
-
-           
-            
-            return a*b;
-        }
-
-        public static int del(int a, int b)
-        {
-
-
-            
-            
-            return a/b;
-        }
-        
-    }
-
-
+def write_msg(user_id, message, random_id):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random_id})
     
-        
-        
-    class Program
-    {
-        
+# API-ключ созданный ранее
+token = "Ваш токен"
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
 
-        
-        static void Main(string[] args)
-        {
-            
-            
-            Console.WriteLine(Calculator.mi(2, 2));
-            Console.WriteLine(Calculator.del(2, 2));
-            Console.WriteLine(Calculator.umn(2, 2));
-            Console.WriteLine(Calculator.Summa(2, 2));
-            Console.ReadKey();
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
 
-        }
-    }
-}
+# Основной цикл
+print("Server started")
+keyboard = open("keyboard.json", "r", encoding = "UTF-8").read()
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            
+            print('New message:')
+            print(f'For me by: {event.user_id}', end='')
+            bot = VkBot(event.user_id)
+            write_msg(event.user_id, bot.new_message(event.text),event.random_id)
+            
+            print('Text: ', event.text)
+            
+             
+            
+            vk.method('messages.send', {'user_id': event.user_id,'keyboard':keyboard, 'message': 'ыыы', 'random_id': 0})
+
+            if event.text.lower() == 'саня':
+                
+                vk.method('messages.send', {'user_id': event.user_id, 'attachment':'photo144827456_457246174', 'message': 'ооо', 'random_id': 0})
+            
+
 
